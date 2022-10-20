153ff567c72e6ee20209da0afd284a570ebbd362
ag-rishbah <agrishabh.ag@gmail.com>
2022-10-20 22:01:35 +0530

    Added Student Controller. Get All student Call is added.

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+           var students = await studentRepository.GetStudentsAsync();
+
+            List<Student> domainStudentModel = mapper.Map<List<Student>>(students);
+
+            return Ok(domainStudentModel);
+        }
+    }
+}
